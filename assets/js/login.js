dd51695e4807a8951fae3db6e618db53857b1ca4
Othman <acerxr@gmail.com>
2023-04-12 00:15:17 +0300

    Add login functionality

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,12 +1,40 @@
-let login = document.querySelector(".login");
-let loginContainer = document.querySelector(".login-container");
-let body = document.querySelector("body");
+const url = 'https://raw.githubusercontent.com/cmps350s2023/cmps350-content-m/main/project/users.json'
 
-login.addEventListener("click", async() => {
+async function loadPage(pageUrl) {
+    //Load Login Form
     const mainContent = document.querySelector('.main');
-    const page = await fetch("../../login.html")
+    const page = await fetch(pageUrl)
     const pageHTMLContent = await page.text()
     mainContent.innerHTML = pageHTMLContent;
     mainContent.style.display = "flex"
     mainContent.style.justifyContent = "center"
-});
+
+    //HTML elemtns
+    let emailInput = document.querySelector(".email-input")
+    let passwordInput = document.querySelector(".password-input")
+    let loginBtn = document.querySelector(".login-btn")
+
+    //Get Users Data
+    let request = await fetch(url)
+    let data = await request.json()
+
+    // Action done after click the login button
+    loginBtn.addEventListener("click", (e) => {
+        e.preventDefault()
+
+        if (checker(emailInput.value, passwordInput.value, data))
+            alert("yes")
+        else
+            alert("noo")
+    })
+}
+
+// Loop over the users object array and check for match
+function checker(email, password, data) {
+    for (let user of data) {
+        if (user.email == email && user.password == password)
+            return true
+    }
+    return false
+}
+
