abea201dc4c1fd9fa05c6c80a1bacc962dfa6619
Othman <acerxr@gmail.com>
2023-04-13 08:41:44 +0300

    Update Paper

--- assets/js/paper.js
+++ assets/js/paper.js
@@ -1,8 +1,11 @@
 export async function paperSubmission() {
 
+    //Load page content 
     const mainContent = document.querySelector('.main');
     const page = await fetch("../../paper.html")
     const pageHTMLContent = await page.text()
+
+    //CSS Style for loaded contnets
     mainContent.innerHTML = pageHTMLContent;
     mainContent.style.display = "flex"
     mainContent.style.justifyContent = "center"
@@ -11,9 +14,16 @@ export async function paperSubmission() {
     //HTML Element Selection
     let titleInput = document.querySelector(".input-title")
     let abstractInput = document.querySelector(".input-abstract")
+    let authorsList = document.querySelector("#authors-list")
+    let presentersList = document.querySelector("#presenters-list")
     let submitBtn = document.querySelector(".submit")
 
-    console.log(titleInput);
-    console.log(abstractInput);
-    console.log(submitBtn);
+    //Fetch data
+    let request = await fetch("https://raw.githubusercontent.com/cmps350s2023/cmps350-content-m/main/project/users.json")
+    let data = await request.json()
+
+    // Append authors name to the list
+    authorsList.innerHTML = data.map(author => `<option value="${author.first_name}">${author.first_name} ${author.last_name}</option>`)
+
+    
 }
