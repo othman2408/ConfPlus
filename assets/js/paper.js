47afe82dae52dda8a04d1e4c0037d12b82916e90
Othman <acerxr@gmail.com>
2023-04-24 17:27:29 +0300

    Update paper.js

--- assets/js/paper.js
+++ assets/js/paper.js
@@ -83,7 +83,7 @@ export async function paperSubmission() {
 
     //Delete the author from the authors container and the authors array and the presenters drop list after clicking the delete button
     let deleteAuthorBtns = document.querySelectorAll(".targetAuthor i")
-    deleteAuthor(deleteAuthorBtns, authorsArray, presentersDropList, presentersContainer, presentersList)
+    deleteAuthor(deleteAuthorBtns, authorsArray, presentersDropList, presentersContainer)
 
     //Add the new author to the presenters drop list
     appendAuthorsToPresenterList(authorsArray, presentersDropList)
@@ -94,7 +94,8 @@ export async function paperSubmission() {
 
   //Add the select presnter to the presenters container
   presentersDropList.addEventListener("change", (e) => {
-    presentersContainer.innerHTML = `<li class = "selectedPresenter" data-id = ${e.target.options[e.target.selectedIndex].getAttribute("data-id")}>${e.target.options[e.target.selectedIndex].innerHTML
+    let id = e.target.options[e.target.selectedIndex].getAttribute("data-id");
+    presentersContainer.innerHTML = `<li class = "selectedPresenter" data-id = ${id}>${e.target.options[e.target.selectedIndex].innerHTML
       }<i class="ti ti-x"></i></li>`;
     //get thhe id from the attribute data-id
     console.log();
@@ -121,24 +122,23 @@ function appendAuthorsToAuthorList(authorsArray, authorsContainer) {
     `<li class = "targetAuthor" data-id = ${author.id}>${author.fname} ${author.lname}<i class="ti ti-x"></i></li>`).join("");
 }
 
-function deleteAuthor(deleteAuthorBtns, authorsArray, presentersDropList, presentersContainer, presentersList) {
+function deleteAuthor(deleteAuthorBtns, authorsArray, presentersDropList, presentersContainer) {
 
   deleteAuthorBtns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
 
       //ID of the author to be deleted
       let id = e.target.parentElement.dataset.id;
-      console.log(id);
-
       //remove the author from the authors array by splicing it
       authorsArray.splice(authorsArray.findIndex((author) => author.id == e.target.parentElement.dataset.id), 1);
       e.target.parentElement.remove();
 
       //Remove the author from the presenters drop list
       presentersDropList.innerHTML = `<option value="" selected disabled>Select Affiliation</option>` +
-        authorsArray.map((author) => `<option value="${author.id}">${author.fname} ${author.lname}</option>`).join("");
+        authorsArray.map((author) => `<option data-id = ${author.id} value="${author.id}">${author.fname} ${author.lname}</option>`).join("");
 
       //Remove the added presenter from the presenters container if the deleted author was the presenter
+      //Not Working
       let selectedPresenter = presentersContainer.querySelector(".selectedPresenter");
       if (selectedPresenter.dataset.id == id) {
         selectedPresenter.remove();
