827811481b3ded8f64c2b1dba8a8866d51bec4d8
Omar Shalaby <114592773+Omar-os1902693@users.noreply.github.com>
2023-04-27 23:47:08 +0300

    Create locations.js and conference-dates.js

---
+++ assets/json/locations.js
@@ -0,0 +1,17 @@
+[
+    {
+        "location": "Female Engineering Building C07-145"
+    },
+    {
+        "location": "CSE Meeting Room BCR-E104"
+    },
+    {
+        "location": "QU Library B13-201"
+    },
+    {
+        "location": "H07-G222"
+    },
+    {
+        "location": "B05-231"
+    }
+]
