d042e41a886c6f04e8c3fb1f4208e7c4096f046a
Othman <acerxr@gmail.com>
2023-04-28 19:01:15 +0300

    Update paper.js

--- assets/js/paper.js
+++ assets/js/paper.js
@@ -183,12 +183,10 @@ function deleteAuthor(
 
 // Assign tan random reviewer to the paper
 function assignReviewr(reviewersArray) {
-  let reviewer1 = reviewersArray[Math.floor(Math.random() * reviewersArray.length)];
-  let reviewer2 = reviewersArray[Math.floor(Math.random() * reviewersArray.length)];
-  while (reviewer2 === reviewer1) {
-    reviewer2 = reviewersArray[Math.floor(Math.random() * reviewersArray.length)];
-  }
-  return [reviewer1, reviewer2];
+  return [
+    reviewersArray[Math.floor(Math.random() * reviewersArray.length)],
+    reviewersArray[Math.floor(Math.random() * reviewersArray.length)],
+  ];
 }
 
 // Raetrive the reviewers from the repo
